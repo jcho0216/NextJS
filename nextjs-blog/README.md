8eb78b50c174d002a6f1e784e97ce3b989c8086c
Jun Joy <chojuneseo1@gmail.com>
2021-07-03 23:00:37 +0900

    update; nextjs-blog

--- nextjs-blog/README.md
+++ nextjs-blog/README.md
@@ -1,1 +1,1 @@
-NextJs Blog tutorial 
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -1,5 +1,5 @@
-import '../pages/styles/global.css'
+import '../styles/global.css'
 
-export default function App({ Component, pageProps}){
-    return <Component {...pageProps}/>
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
